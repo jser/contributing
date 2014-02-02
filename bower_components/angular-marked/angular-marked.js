ff1a1f1b37b328b0f2b109e316b00d0fe1b62894
azu <azuciao@gmail.com>
2014-02-02 17:34:52 +0900

    fix(bower): add to index

---
+++ bower_components/angular-marked/angular-marked.js
@@ -0,0 +1,49 @@
+/*
+ * angular-marked v0.0.1
+ * (c) 2013 J. Harshbarger
+ * License: MIT
+ */
+
+/* jshint undef: true, unused: true */
+/* global angular:true */
+/* global marked:true */
+
+(function () {
+	'use strict';
+
+  var app = angular.module('hc.marked', []);
+
+  app.constant('marked', window.marked);
+
+  // TODO: filter tests */
+  //app.filter('marked', ['marked', function(marked) {
+	//  return marked;
+	//}]);
+
+  app.directive('marked', ['marked', function (marked) {
+    return {
+      restrict: 'AE',
+      replace: true,
+      scope: {
+        opts: '=',
+        marked: '='
+      },
+      link: function (scope, element, attrs) {
+        var value = scope.marked || element.text() || '';
+        set();
+
+        function set() {
+        	element.html(marked(value, scope.opts || null));
+        }
+        
+        if (attrs.marked) {
+          scope.$watch('marked', function(value) {
+            element.html(marked(value || '', scope.opts || null));
+          });        	
+        }
+
+      }
+    };
+  }]);
+
+}());
