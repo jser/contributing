06131ec90d6243a221ed584251b63afa029819d0
azu <azuciao@gmail.com>
2014-02-02 14:59:27 +0900

    feat(index) : 関連サイトの入力項目を追加

--- src/controllers/data-form.js
+++ src/controllers/data-form.js
@@ -3,5 +3,16 @@
 module.exports = function (app) {
     app.controller("dataFormController", function ($scope, dataFormModel) {
         $scope.data = dataFormModel;
+        $scope.addNewRelatedSite = function () {
+            var newItemNo = dataFormModel.relatedSites.length + 1;
+            dataFormModel.relatedSites.push({});
+        };
+        $scope.isShowLabel = function (choice) {
+            return true;
+        };
+        $scope.isShowButton = function (choice) {
+            return dataFormModel.relatedSites.length;
+        };
     });
+
 };
--- src/models/data-form.js
+++ src/models/data-form.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
     app.service("dataFormModel", function () {
         this.tags = ["JavaScript", "Angular"];
+        this.relatedSites = [];
     });
 };
