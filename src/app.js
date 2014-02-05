252283285accef5daf13395ec30da14d5a727089
azu <azuciao@gmail.com>
2014-02-05 21:47:59 +0900

    feat(location): fix content & tags

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ var app = angular.module('app', ['ngTagsInput', 'hc.marked']).config([
     '$locationProvider', function ($locationProvider) {
         $locationProvider.html5Mode(true);
     }
-])
+]);
 require("./controllers/data-form-controller")(app);
 require("./models/data-form-model")(app);
 // jser
--- src/models/data-form-model.js
+++ src/models/data-form-model.js
@@ -1,13 +1,18 @@
 "use strict";
 module.exports = function (app) {
+    var splitTags = function (tags) {
+        return tags.split(",").filter(function(ele){
+            return ele.length > 0;
+        });
+    };
     var dataFormService = function ($location) {
         var query = $location.search();
         return {
             title: query.title || "",
             url: query.url || "",
-            content: "",
+            content: query.content || "",
             date: new Date(),
-            tags: [],
+            tags: query.tags ? splitTags(query.tags): [],
             "related-links": [],
             insertNewRelatedSite: function () {
                 this["related-links"].push({});
